0df7dde61db30172ae2d4efafcbbba28fff78cf4
Jerome-debug <68271511+Jerome-debug@users.noreply.github.com>
2023-01-13 11:30:57 +0300

    Improved predict button function

--- app.py
+++ app.py
@@ -4,19 +4,29 @@ import numpy as np
 import pickle as pkl
 from sklearn.preprocessing import MinMaxScaler
 
+
 scal=MinMaxScaler()
 
-#Load the saved model
+#Load the saved pickle model
 model=pkl.load(open("model.pkl","rb"))
 
-#st.set_page_config(page_title="Healthy Heart App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
-def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal):   
-    # Pre-processing user input   
-    if sex=="male":
-        sex=1 
-    else:
-        sex=0
+
+
+
+
+st.set_page_config(page_title=" Heart Disease Detection App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
+
+@st.cache()
+ 
+     
+def prediction(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal ):   
+
     
+    # Pre-processing categorical columnns from user input
+    if   sex=="male":
+         sex=0 
+    else: sex= 1
+
     if cp=="Typical angina":
         cp=0
     elif cp=="Atypical angina":
@@ -24,7 +34,7 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
     elif cp=="Non-anginal pain":
         cp=2
     elif cp=="Asymptomatic":
-        cp=2
+        cp=3
     
     if exang=="Yes":
         exang=1
@@ -56,21 +66,23 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
         restecg=1
     elif restecg=="Possible or definite left ventricular hypertrophy":
         restecg=2
-
+        #preprocessing user  input
     user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
     user_input=np.array(user_input)
     user_input=user_input.reshape(1,-1)
     user_input=scal.fit_transform(user_input)
     prediction = model.predict(user_input)
 
-    return prediction
-
-
-
+    if prediction == 1:
+        pred="Warning! You have high risk of getting a heart attack!"
+    else:
+        pred ="You have a lower risk of getting a heart disease!"
+##Creating the main page
 def main(): 
+
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Heart Prediction ML App</h1> 
+    <h1 style ="color:black;text-align:center;">Streamlit Heart Disease Prediction ML App</h1> 
     </div> 
     """
 
@@ -79,37 +91,41 @@ def main():
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html = True) 
     st.subheader('by Bundi Kirimi ')
-        
-    # following lines create boxes in which user can enter data required to make prediction
-    age=st.selectbox ("Age",range(1,121,1))
-    sex = st.radio("Select Gender: ", ('male', 'female'))
-    cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
-    trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
-    restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
-    chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
-    fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
-    thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
-    exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
-    oldpeak=st.number_input('Oldpeak')
-    slope = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
-    ca=st.selectbox('Number of Major Vessels Colored by Flourosopy',range(0,5,1))
-    thal=st.selectbox('Thalium Stress Result',range(1,8,1))
     
-    #user_input=preprocess(sex,cp,exang, fbs, slope, thal )
-    pred=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+# following lines create boxes in which user can enter data required to make prediction
+age=st.selectbox ("Age",range(1,121,1))
+sex = st.radio("Select Gender: ",('male', 'female'))
+cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
+trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
+restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
+fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
+exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
+oldpeak=st.number_input('Oldpeak')
+slope = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
+ca=st.selectbox('Number of Major Vessels Colored by Flourosopy',range(0,5,1))
+thal=st.selectbox('Thalium Stress Result',range(1,8,1))
+result =""
+    
 
-    # When "Predict" is clicked, make prediction
-    # TODO figure out how to use the click event for button
-    if st.button("Predict"):
-      if pred[0]== 0:
-        st.error('Warning! You have high risk of getting a heart attack!')
+if st.button("Predict"): 
+
+    result =prediction(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+    if result == 0:
+        st.write('Warning! You have high risk of getting a heart attack!')
     else:
-        st.success('You have lower risk of getting a heart disease!')
+     
+       st.write("You have a lower risk of getting a heart disease!")
 
-    st.sidebar.subheader("About App")
-    st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a heart disease.")
-    st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy heart")
-    st.sidebar.info("Don't forget to rate this app")
+      
+
+st.sidebar.subheader("About App")
+
+st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a heart disease.")
+st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy heart")
+st.sidebar.info("Don't forget to rate this app")
+
 
 
 feedback = st.sidebar.slider('How much would you rate this app?',min_value=0,max_value=5,step=1)
